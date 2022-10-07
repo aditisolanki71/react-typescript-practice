b82c7d18f2cc528c823169df76b86c5a19e19266
Unknown <adtsolanki22@gmail.com>
2022-10-07 20:01:51 +0530

    Basic Props Type example(string,number,boolean,object,array)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import './App.css';
 import Greet from './components/Greet';
 function App() {
+  const personObject = {
+    first: "Adi",
+    last: "Solanki"
+  }
+  const hobbiesArray = [" singing ", " dancing "," coding "];
+  const personListArray = [
+    {
+      first: "Moni",
+      last: "solanki"
+    },
+    {
+      first: "Arjun",
+      last: "solanki"
+    }
+  ]
   return (
     <div className="App">
-      <Greet />
+      <Greet 
+        name="Aditi" 
+        age={20} 
+        isLoggesIn
+        fullName={personObject}
+        hobbies={hobbiesArray}
+        personList={personListArray}
+      />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,7 +1,37 @@
-const Greet = () => {
+type greetProps = {
+   name: string,
+   age: number,
+   isLoggesIn: boolean,
+   fullName: {
+      first: string,
+      last: string
+   },
+   hobbies: string[],
+   personList: {
+      first:string,
+      last: string
+   }[]
+}
+const Greet = (props: greetProps) => {
    return (
       <div>
-         <h1>Welcome Aditi</h1>
+         {props.isLoggesIn ? (
+            <>
+               <h1>Hello {props.name}</h1>
+               <h2>Welcome {props.fullName.first} {props.fullName.last}</h2>
+               <h2>Age {props.age}</h2>
+               <h3>My Hobbies are:- {props.hobbies} </h3>
+               <ul>
+                  {props.personList.map((person,index) => (
+                     <>
+                        <li key={index}>{person.first} - {person.last}</li>
+                     </>
+                     )
+                  )}
+               </ul>
+            </>   
+         ) : 
+         <h1>Welcome Guest!!!</h1>}
       </div>
    )
 }
